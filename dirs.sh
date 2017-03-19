7fedcb12fc0613658678e2ccae126211f8e9b9c2
Bianca Santiago <bianca.santiago72@gmail.com>
2017-03-19 10:18:32 -0300

    Arquivo shell com os comandos para construção dos repositórios

---
+++ dirs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script para criacao de diretorios para estruturacao do projeto
+
+# Diretorio para armazenamento dos executaveis finais
+mkdir -p bin
+
+# Diretorio para armazenamento dos objetos
+mkdir -p build
+
+# Diretorio para armazenamento de documentacao gerada automaticamente
+mkdir -p doc
+
+# Diretorio para armazenamento de arquivos de cabecalho (.h)
+mkdir -p include
+
+# Diretorio para armazenamento de bibliotecas a serem ligadas ao programa
+mkdir -p lib
+
+# Diretorio para armazenamento de arquivos de corpo (.cpp)
+mkdir -p src
+
+# Diretorio para armazenamento de codigos fonte destinados a teste
+mkdir -p test
