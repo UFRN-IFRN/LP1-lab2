fd41bd3424caef84674e96d34c770e842828ab99
Bianca Santiago <bianca.santiago72@gmail.com>
2017-03-21 10:54:37 -0300

    Correção da Questão 06

--- src/qst06/main06.cpp
+++ src/qst06/main06.cpp
@@ -8,14 +8,18 @@
 
 #include "../../include/qst06/ternaria.h"
 
- /**
+/**
 *@brief Função principal que recebe o elemento a ser buscado.
 */
-int main (int argc, char* argv[]) {
+int main () {
+
+	int x;
+
+	cin >> x;
 
     int v[26] = { 2,5,9,11,13,17,22,24,33,38,39,40,45,56,71,99,110,113,132,155,166,203,211,212,230,233 };
 
-    int key = atoi(argv[1]);
+    int key = x;
 
     if (buscaTernaria (v, 0, 25, key ) == 0) {
         cout << "O elemento " << key << " não faz parte do vetor" << endl;
