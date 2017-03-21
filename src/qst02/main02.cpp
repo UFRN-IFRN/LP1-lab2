57f166f8c579e4bf8a0c6ce6b0ac529872776a12
Bianca Santiago <bianca.santiago72@gmail.com>
2017-03-21 02:55:43 -0300

    Correção da Questão 2 (Tratamento de Exceção 'em caso de n < 0')

--- src/qst02/main02.cpp
+++ src/qst02/main02.cpp
@@ -3,10 +3,14 @@
 int main(){
 	int m, n;
 
-	cout << "Digite dois números naturais positivos: " << endl;
-	cin >> m >> n;
+		cout << "Digite dois números naturais positivos: " << endl;
+		cin >> m >> n;
 
-	cout << "MDC(" << m << ", " << n << ") = " << mdc(m, n) << endl;
-
+	if (n > 0){
+		cout << "MDC(" << m << ", " << n << ") = " << mdc(m, n) << endl;
+	} else {
+		cout << "Valor(es) inválido(s). Por favor, digite valores positivos." << endl;
+		main();
+	}
 	return 0;
 }
